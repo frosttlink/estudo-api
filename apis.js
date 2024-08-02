fcea98b720619f505d8b756bbf4245c8bced70eb
frosttlink <leolorena.alves@gmail.com>
2024-08-02 11:00:32 -0300

    initial commit

---
+++ apis.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const server = express()
+
+server.get('/calculadora/somar/:n1/:n2', (req, resp) => {
+  let n1 = Number(req.params.n1)
+  let n2 = Number(req.params.n2)
+
+  let resultSoma = n1 + n2 
+
+  resp.send({
+    soma: resultSoma
+  })
+})
+
+server.listen(5050, () => console.log("Eta, como sobe"))
