27939b4a8d55b20ac79ebc2d4c02c56eb02a3e42
Carla <carla9lima0carvalho@gmail.com>
2024-08-02 11:52:55 -0300

    subtrair commit

--- apis.js
+++ apis.js
@@ -13,4 +13,16 @@ server.get('/calculadora/somar/:n1/:n2', (req, resp) => {
   })
 })
 
+server.get('/calculadora/subtrair/:n1/:n2', (req,resp) => {
+  let n1 = Number( req.params.n1 )
+  let n2 = Number( req.params.n2 )
+
+  let resultadoSubtrair = n1 - n2
+
+  resp.send ({
+    subtrair : resultadoSubtrair
+})
+})
+
+
 server.listen(5050, () => console.log("Eta, como sobe"))
